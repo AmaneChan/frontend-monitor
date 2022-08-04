798b5b72f5144b441e43b285ec73bf5e143ab867
YG Yang <473611657@qq.com>
2022-08-04 11:50:33 +0800

    登录注册

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,8 +1,21 @@
-<script setup lang="ts">
-</script>
-
 <template>
+<div>
+<login-register></login-register>
+</div>
 </template>
+
+<script setpu lang="ts">
+import LoginRegister from './components/LoginRegister.vue';
+export default{
+    name:'App',
+    components:{
+        LoginRegister
+    },
+    setup(){
+
+    }
+}
+</script>
 
 <style>
 </style>
