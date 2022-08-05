0acf98ab1fd9b843e10eb6fef0b89032afdf2185
LotoLe <1480573732@qq.com>
2022-08-05 13:48:15 +0800

    echarts resue

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -38,9 +38,12 @@
 					</el-row>
 
 					<div class="ma">
-						<el-card >
-						<Chartstwo></Chartstwo>
-					</el-card>
+						<el-card>
+							<Chartstwo :value="parentValue" :option="opt"></Chartstwo>
+						</el-card>
+						<el-card>
+							<Chartstwo :value="dom" :option="opt"></Chartstwo>
+						</el-card>
 					</div>
 				</div>
 			</el-col>
@@ -49,22 +52,122 @@
 </template>
 
 <script lang="ts" setpu>
-import { defineComponent, reactive, ref, toRefs, onMounted ,Ref } from 'vue'
+import { defineComponent, reactive, ref, toRefs, onMounted, Ref } from 'vue'
 import HeadMenu from './components/HeadMenu.vue'
 import SideMenu from './components/SideMenu.vue'
 import Chartsone from './components/Charts_one.vue'
 import Chartstwo from './components/Charts_two.vue'
+
+
 export default {
 	name: 'App',
 	components: {
 		HeadMenu,
 		SideMenu,
 		Chartsone,
-		Chartstwo
+		Chartstwo,
 	},
 
 	setup() {
-		
+		let opt = ref({
+				title: {
+					text: 'xx分析',
+				},
+				tooltip: {
+					trigger: 'axis',
+					axisPointer: {
+						type: 'cross',
+						label: {
+							backgroundColor: '#6a7985',
+						},
+					},
+				},
+				legend: {
+					data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
+				},
+				toolbox: {
+					feature: {
+						saveAsImage: {},
+					},
+				},
+				grid: {
+					left: '3%',
+					right: '4%',
+					bottom: '3%',
+					containLabel: true,
+				},
+				xAxis: [
+					{
+						type: 'category',
+						boundaryGap: false,
+						data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+					},
+				],
+				yAxis: [
+					{
+						type: 'value',
+					},
+				],
+				series: [
+					{
+						name: 'Email',
+						type: 'line',
+						stack: 'Total',
+						areaStyle: {},
+						emphasis: {
+							focus: 'series',
+						},
+						data: [120, 132, 101, 134, 90, 230, 210],
+					},
+					{
+						name: 'Union Ads',
+						type: 'line',
+						stack: 'Total',
+						areaStyle: {},
+						emphasis: {
+							focus: 'series',
+						},
+						data: [220, 182, 191, 234, 290, 330, 310],
+					},
+					{
+						name: 'Video Ads',
+						type: 'line',
+						stack: 'Total',
+						areaStyle: {},
+						emphasis: {
+							focus: 'series',
+						},
+						data: [150, 232, 201, 154, 190, 330, 410],
+					},
+					{
+						name: 'Direct',
+						type: 'line',
+						stack: 'Total',
+						areaStyle: {},
+						emphasis: {
+							focus: 'series',
+						},
+						data: [320, 332, 301, 334, 390, 330, 320],
+					},
+					{
+						name: 'Search Engine',
+						type: 'line',
+						stack: 'Total',
+						label: {
+							show: true,
+							position: 'top',
+						},
+						areaStyle: {},
+						emphasis: {
+							focus: 'series',
+						},
+						data: [820, 932, 901, 934, 1290, 1330, 1320],
+					},
+				],
+			})
+		const parentValue = ref('domid')
+		const dom = ref('dom')
+		return { parentValue,dom,opt }
 	},
 }
 </script>
--- packages/frontend/src/components/Charts_two.vue
+++ packages/frontend/src/components/Charts_two.vue
@@ -1,118 +1,35 @@
 <template>
 	<div>
-		<div id="Charts-two"></div>
+		<div :id="id" class="echats"></div>
 	</div>
 </template>
 
 <script>
+import { onMounted, getCurrentInstance, ref } from 'vue'
+import * as echarts from 'echarts'
 export default {
 	name: 'Charts-two',
-	mounted() {
-		// 基于准备好的dom，初始化echarts实例
-		var myChart = this.$echarts.init(document.getElementById('Charts-two'))
-		// 绘制图表
-		myChart.setOption({
-			title: {
-				text: 'xx分析',
-			},
-			tooltip: {
-				trigger: 'axis',
-				axisPointer: {
-					type: 'cross',
-					label: {
-						backgroundColor: '#6a7985',
-					},
-				},
-			},
-			legend: {
-				data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
-			},
-			toolbox: {
-				feature: {
-					saveAsImage: {},
-				},
-			},
-			grid: {
-				left: '3%',
-				right: '4%',
-				bottom: '3%',
-				containLabel: true,
-			},
-			xAxis: [
-				{
-					type: 'category',
-					boundaryGap: false,
-					data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-				},
-			],
-			yAxis: [
-				{
-					type: 'value',
-				},
-			],
-			series: [
-				{
-					name: 'Email',
-					type: 'line',
-					stack: 'Total',
-					areaStyle: {},
-					emphasis: {
-						focus: 'series',
-					},
-					data: [120, 132, 101, 134, 90, 230, 210],
-				},
-				{
-					name: 'Union Ads',
-					type: 'line',
-					stack: 'Total',
-					areaStyle: {},
-					emphasis: {
-						focus: 'series',
-					},
-					data: [220, 182, 191, 234, 290, 330, 310],
-				},
-				{
-					name: 'Video Ads',
-					type: 'line',
-					stack: 'Total',
-					areaStyle: {},
-					emphasis: {
-						focus: 'series',
-					},
-					data: [150, 232, 201, 154, 190, 330, 410],
-				},
-				{
-					name: 'Direct',
-					type: 'line',
-					stack: 'Total',
-					areaStyle: {},
-					emphasis: {
-						focus: 'series',
-					},
-					data: [320, 332, 301, 334, 390, 330, 320],
-				},
-				{
-					name: 'Search Engine',
-					type: 'line',
-					stack: 'Total',
-					label: {
-						show: true,
-						position: 'top',
-					},
-					areaStyle: {},
-					emphasis: {
-						focus: 'series',
-					},
-					data: [820, 932, 901, 934, 1290, 1330, 1320],
-				},
-			],
+	props: ['value', 'option'],
+	setup(props) {
+		let id = ref(props.value)
+		const ins = getCurrentInstance()
+		onMounted(() => {
+			console.log(id.value)
+			let dom = document.getElementById(id.value)
+			let myChart = null;
+			myChart = echarts.init(dom)
+			myChart.setOption(props.option)
+			console.log(props)
 		})
+		return {
+			id,
+		}
 	},
 }
 </script>
 
 <style>
-#Charts-two {
+.echats {
 	height: 400px;
 	width: 100%;
 }
