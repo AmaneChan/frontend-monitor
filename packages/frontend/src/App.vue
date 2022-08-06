d65cc5ea05f87605dcd14b81c1aa3ca94a1d82a7
Amane <AmaneChan@users.noreply.github.com>
2022-08-06 11:23:30 +0800

    format code and some fix

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,21 +1,20 @@
 <template>
-<div>
-<login-register></login-register>
-</div>
+	<div>
+		<LoginRegister></LoginRegister>
+	</div>
 </template>
 
-<script setpu lang="ts">
-import LoginRegister from './components/LoginRegister.vue';
-export default{
-    name:'App',
-    components:{
-        LoginRegister
-    },
-    setup(){
-
-    }
-}
+<script setup lang="ts">
+import LoginRegister from './components/LoginRegister.vue'
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;700&display=swap');
+
+body {
+	margin: 0;
+	padding: 0;
+
+	font-family: 'Noto Sans SC', sans-serif;
+}
 </style>
