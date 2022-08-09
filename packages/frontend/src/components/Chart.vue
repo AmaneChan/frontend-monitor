70e1275cff6e41dd63de642902dd2b35161c02df
Amane <AmaneChan@users.noreply.github.com>
2022-08-09 19:06:31 +0800

    simplify codes

--- packages/frontend/src/components/Chart.vue
+++ packages/frontend/src/components/Chart.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { onMounted, getCurrentInstance, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import * as echarts from 'echarts'
 
 const props = defineProps(['value', 'option', 'width', 'height'])
