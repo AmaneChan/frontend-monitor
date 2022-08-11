16642c4d0113436c535352d58a4619654e3c06d7
superyeda <2556999573@qq.com>
2022-08-11 12:33:10 +0800

    用户行为模块

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup lang="ts">
-import LoginRegister from './components/LoginRegister.vue'
+// import LoginRegister from './components/LoginRegister.vue'
 </script>
 
 <style>
