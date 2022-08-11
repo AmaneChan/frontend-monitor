b1d76ed1508a282781fec31a8881099d0b206581
Amane <70516075+AmaneChan@users.noreply.github.com>
2022-08-11 19:30:45 +0800

    feat(frontend): finish exception, performance, behavior view
    * new echarts
    
    * Exception and Performance
    
    * use style bind instead of dom operations
    
    * simplify codes
    
    * charts
    
    * 用户行为模块
    
    * remove package-lock.json
    
    * install dependency
    
    * tweak style
    
    Co-authored-by: LotoLe <1480573732@qq.com>
    Co-authored-by: Amane <AmaneChan@users.noreply.github.com>
    Co-authored-by: superyeda <2556999573@qq.com>

--- packages/frontend/src/components/Chart.vue
+++ packages/frontend/src/components/Chart.vue
@@ -1,31 +1,45 @@
 <script lang="ts" setup>
-import { onMounted, getCurrentInstance, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import * as echarts from 'echarts'
+import ElementResize from 'element-resize-detector'
 
-const props = defineProps(['value', 'option'])
+const props = defineProps(['value', 'option', 'width', 'height'])
 
+// var dt = new Date();
+// console.log(dt.getTime());
+// const id = ref(dt.getTime()+'')
 const id = ref(props.value)
-const ins = getCurrentInstance()
+
 onMounted(() => {
-	console.log(id.value)
 	const dom = document.getElementById(id.value)
+
 	if (dom) {
-		const myChart = echarts.init(dom)
+		let myChart = echarts.init(dom)
 		myChart.setOption(props.option)
-		console.log(props)
+		//图表随父组件改变而改变
+		var elementResize = ElementResize({
+			strategy: 'scroll', //  推荐监听滚动，提升性能
+			callOnAdd: true, // 添加侦听器时是否应调用,默认true
+		})
+		elementResize.listenTo(dom, function (element) {
+			// 当元素尺寸发生改变是会触发此事件，刷新图表
+			echarts.init(dom).resize()
+		})
+		//图表随浏览器窗口改变而改变
+		// window.addEventListener("resize", function () {
+		// 	myChart.resize();
+		// });
 	}
 })
 </script>
 
 <template>
 	<div>
-		<div :id="id" class="echats"></div>
+		<div
+			:style="{ height: props.height || '300px', width: props.width || '100%' }"
+			:id="id"
+		></div>
 	</div>
 </template>
 
-<style>
-.echats {
-	height: 300px;
-	width: 100%;
-}
-</style>
+<style></style>
